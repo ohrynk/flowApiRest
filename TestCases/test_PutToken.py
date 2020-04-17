97974a2d5686fb3ab6371a66be896928d1661106
ohrynk <60706304+ohrynk@users.noreply.github.com>
2020-04-17 10:50:52 -0300

    test cases

---
+++ TestCases/test_PutToken.py
@@ -0,0 +1,17 @@
+import requests
+import json
+import jsonpath
+
+# API Get Token
+url = "http://127.0.0.1:8000/v1/api-token-auth/"
+
+
+def test_post_token():
+    file = open("body.json")
+    json_input = file.read()
+    request_json = json.loads(json_input)
+    response = requests.post(url, request_json)
+    print(response.content)
+    response_json = json.loads(response.text)
+    token = jsonpath.jsonpath(response_json, 'token')
+    assert token[0] == '3289b551c28b65b32fe6c8b94215121d2bc36b7c'
