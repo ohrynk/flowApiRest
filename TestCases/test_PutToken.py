5d89bdb5e8a9e24f0a80be6f76df78b898b9c195
ohrynk <60706304+ohrynk@users.noreply.github.com>
2020-04-17 17:49:30 -0300

    minor

--- TestCases/test_PutToken.py
+++ TestCases/test_PutToken.py
@@ -23,16 +23,25 @@ def test_post_token_other():
     request_json = json.loads(json_input)
     response = requests.post(url, request_json)
     print(response.content)
-    response_json = json.loads(response.text)
-    token = jsonpath.jsonpath(response_json, 'token')
     assert response.status_code == 200
 
-
-def test_get_insert_paises():
+def test_post_insert_paises():
     ruta = "http://127.0.0.1:8000/v1/paises/"
-    pais='Bolivia'
-    sigla='BO'
+    pais='Uruguay'
+    sigla='UR'
     body = json.dumps({'nombre': pais, 'sigla': sigla})
     headers = {'Content-Type':'application/json','Authorization':'Token 3289b551c28b65b32fe6c8b94215121d2bc36b7c'}
     response = requests.post(ruta, body, headers=headers)
     assert response.status_code == 201
+
+def test_get_paises():
+    ruta = "http://127.0.0.1:8000/v1/paises/"
+    headers = {'Content-Type': 'application/json', 'Authorization': 'Token 3289b551c28b65b32fe6c8b94215121d2bc36b7c'}
+    response = requests.get(ruta, headers=headers)
+    response_json = json.loads(response.text)
+    count = jsonpath.jsonpath(response_json, 'count')
+    # probar la cantidad
+    assert count[0] ==5
+
+
+
